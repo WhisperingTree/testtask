5d74788849651c8e4202a9b01e0e1162cb728ba9
WhisperingTree <renegate1984@gmail.com>
2015-09-19 21:35:16 +0300

    Missing files/folders added!

---
+++ testtask.ua/core/cache/action_map/mgr/actions.cache.php
@@ -0,0 +1,28 @@
+<?php  return array (
+  2 => 
+  array (
+    'id' => '2',
+    'namespace' => 'quip',
+    'controller' => 'index',
+    'haslayout' => '1',
+    'lang_topics' => 'quip:default,file',
+    'assets' => '',
+    'help_url' => '',
+    'namespace_name' => 'quip',
+    'namespace_path' => 'G:/OpenServer/domains/testtask.ua/core/components/quip/',
+    'namespace_assets_path' => NULL,
+  ),
+  1 => 
+  array (
+    'id' => '1',
+    'namespace' => 'tagger',
+    'controller' => 'index',
+    'haslayout' => '1',
+    'lang_topics' => 'tagger:default',
+    'assets' => '',
+    'help_url' => '',
+    'namespace_name' => 'tagger',
+    'namespace_path' => 'G:/OpenServer/domains/testtask.ua/core/components/tagger/',
+    'namespace_assets_path' => '{assets_path}components/tagger/',
+  ),
+);
